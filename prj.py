2a8d1bddb54295fde478a2aa0657fbfcf331e4cd
George Papadopoulos <papadopoulos.g@gmail.com>
2017-11-17 03:00:10 +0200

    Update and rename prj.txt to prj.py

---
+++ prj.py
@@ -0,0 +1,10 @@
+import sys
+
+print "Hello World!" #USUCK
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+raw_input()
