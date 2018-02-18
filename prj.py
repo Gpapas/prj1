be11e0cabae0f149d9a1ca6163879a110e729775
Georgios Papadopoulos <papadopoulos.g@gmail.com>
2018-02-18 22:22:01 +0200

    edited

--- prj.py
+++ prj.py
@@ -1,10 +1,6 @@
 import sys
 
-print "Hello World!" #USUCK
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
-raw_input()
+print ("Hello World!") #USUCK
+print ("Hello Again")
+print ('Yay! Printing.')
+#raw_input()
